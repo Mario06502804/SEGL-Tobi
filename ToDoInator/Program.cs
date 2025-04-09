e2b3a2b1283e0551896d4406e9a099221a106c2f
Mario06502804 <mario06502804@icloud.com>
2025-04-09 22:46:26 +0200

    Update Program.cs

--- ToDoInator/Program.cs
+++ ToDoInator/Program.cs
@@ -1,44 +1,45 @@
-﻿
-Console.WriteLine("Willkommen bei ToDoInator");
-var ToDoList = new List<ToDoItem>();
+﻿﻿Console.WriteLine("Willkommen beim Todoinator!");
+// var meineCooleListe = new List<string>();
+var todoList = new List<TodoItem>();
 while (true)
 {
-    foreach (var Item in ToDoList)
+    for (int i = 0; i < todoList.Count; i++)
     {
-        Console.WriteLine(Item);
+        Console.WriteLine($"Index {i + 1}: {todoList[i]}");
     }
-    Console.WriteLine(value: """
-        1. Neues ToDo
-        2. ToDo abhaken
-        """);
+    Console.WriteLine("""
+    1. Neues Todo
+    2. Todo abhaken
+    """);
     var userMenuChoice = Console.ReadLine();
     switch (userMenuChoice)
     {
         case "1":
-            Console.WriteLine("Was ist das ToDo?");
-            var toDoDescription = Console.ReadLine();
+            // Todo erstellen
+            Console.WriteLine("Was ist das Todo?");
+            var todoDescription = Console.ReadLine();
             Console.WriteLine("""
-                Wie wichtig? 
-                1 => Hohe Prio
-                2 => Normal Prio
-                3 => Niedrige prio
-                """);
-            var toDoPio = Console.ReadLine();
-            var newTodo = new ToDoItem();
-            newTodo.Description = toDoDescription;
-            newTodo.Prio = Convert.ToInt32(toDoPio);
-            ToDoList.Add(newTodo);
+             Wie wichtig?
+             1 => Hohe Prio
+             2 => Normale Prio
+             3 => Niedrige Prio
+             """);
+            var todoPrio = Console.ReadLine();
+            var newTodo = new TodoItem();
+            newTodo.Description = todoDescription;
+            newTodo.Prio = Convert.ToInt32(todoPrio);
+            todoList.Add(newTodo);
             break;
         case "2":
-            // todo abhaken
-            Console.WriteLine("Welches ToDo möchtest du Löschen?");
-            var indexToDelet = Convert.ToInt32(Console.ReadLine());
-            ToDoList.RemoveAt(indexToDelet);
+            // Todo abhaken
+            Console.WriteLine("Welches Todo möchtest do löschen?");
+            var indexToDelete = Convert.ToInt32(Console.ReadLine());
+            todoList.RemoveAt(indexToDelete - 1);
             break;
         default:
-            // nicht definireter Input eg."hamster"
-
+            // Nicht definierter Input eg. "hamster"
             break;
     }
+
     Console.Clear();
 }
